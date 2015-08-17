59ad8f403da04dcf6989ab1abd118a7bfc38d5be
Matt Hampel <matth@localdata.com>
2015-08-17 16:54:21 -0400

    Initial Commit

---
+++ Solid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Solid
+//
+//  Created by Matthew Hampel on 8/17/15.
+//  Copyright © 2015 Matthew Hampel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
